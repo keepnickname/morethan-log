aafe65a12b242f3347f3b8bea20cbd1c1725c8c1
Keepnickname <keepnickname@gmail.com>
2024-09-17 13:58:35 +0800

    update config

--- site.config.js
+++ site.config.js
@@ -12,7 +12,7 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `morethan-log(Not me)`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
